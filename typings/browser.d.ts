b8839ff9e4b8a11183739466d07202f3704d69ed
Rainey <515385036@qq.com>
2016-04-19 16:52:55 +0800

    修复typings报错问题

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/cordova/index.d.ts" />
-/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
